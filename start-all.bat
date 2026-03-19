5fbf993e98c428ff0109e479c86d5bda5f3f0e38
oussema Mhamed <120645606+oussema445@users.noreply.github.com>
2026-03-19 23:15:57 +0300

    Add start script1

--- start-all.bat
+++ start-all.bat
@@ -1,30 +1,28 @@
 @echo off
-echo 🚀 Lancement HR Platform...
+echo Lancement HR Platform...
 
-start "AuthService" cmd /k "cd C:\Dev\HRPlatform\AuthService && dotnet run"
+start "AuthService" cmd /k "cd /d C:\Dev\HRPlatform\AuthService && dotnet run"
 timeout /t 3
 
-start "EmployeeService" cmd /k "cd C:\Dev\HRPlatform\EmployeeService && dotnet run"
+start "EmployeeService" cmd /k "cd /d C:\Dev\HRPlatform\EmployeeService && dotnet run"
 timeout /t 3
 
-start "LeaveService" cmd /k "cd C:\Dev\HRPlatform\LeaveService && dotnet run"
+start "LeaveService" cmd /k "cd /d C:\Dev\HRPlatform\LeaveService && dotnet run"
 timeout /t 3
 
-start "PayrollService" cmd /k "cd C:\Dev\HRPlatform\PayrollService && dotnet run"
+start "PayrollService" cmd /k "cd /d C:\Dev\HRPlatform\PayrollService && dotnet run"
 timeout /t 3
 
-start "DashboardService" cmd /k "cd C:\Dev\HRPlatform\DashboardService && dotnet run"
+start "DashboardService" cmd /k "cd /d C:\Dev\HRPlatform\DashboardService && dotnet run"
 timeout /t 3
 
-start "NotificationService" cmd /k "cd C:\Dev\HRPlatform\NotificationService && dotnet run"
+start "NotificationService" cmd /k "cd /d C:\Dev\HRPlatform\NotificationService && dotnet run"
 timeout /t 3
 
-start "ApiGateway" cmd /k "cd C:\Dev\HRPlatform\ApiGateway && dotnet run"
+start "ApiGateway" cmd /k "cd /d C:\Dev\HRPlatform\ApiGateway && dotnet run"
 timeout /t 3
 
-start "Frontend" cmd /k "cd C:\Dev\HRPlatform\HRFrontend && ng serve"
+start "Frontend" cmd /k "cd /d C:\Dev\HRPlatform\HRFrontend && ng serve"
 
-echo ✅ Tous les services sont lancés !
-echo Frontend: http://localhost:4200
-echo Gateway:  http://localhost:5000
+echo Tous les services sont lances !
 pause
